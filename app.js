8f95746c2d91420370435aefa86702690b43e150
coalyer <liyang_qifengle@126.com>
2022-10-27 21:17:08 +0800

    feat: 1.搭建唯一的shell调试入口 node app.js(基于nodejs) 2.搭建http服务器(基于nodejs) 3.搭建唯一的路由入口

---
+++ app.js
@@ -0,0 +1,11 @@
+//starter-kit server
+
+var Router = require('easy-router')
+
+var port = 3030
+
+Router()
+  .setMap('**/*', '**/*')
+  .listen(port)
+
+console.log('http://127.0.0.1:' + port + '/menu.html')
